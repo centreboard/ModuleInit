7d771085a2ebc534baa618cc040d9495dc9b4ff1
Matthew R Johnson <matthew@centreboard.net>
2021-04-22 20:49:26 +0100

    Add example failing due to CefSharp ModuleInitializerAttribute

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        [ModuleInitializer]
+        internal static void ModuleInitializer() {
+            Console.WriteLine("Class Lib ModuleInitializer");
+        }
+    }
+    
+    
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using ClassLibrary;
+
+namespace ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            new Class1();
+        }
+
+        [ModuleInitializer]
+        internal static void ModuleInitializer() {
+            Console.WriteLine("Console App ModuleInitializer");
+        }
+    }
+}
